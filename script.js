30e2cf8dac0c998acab08a3fea8082580784d601
Adam <92753806+AdamUbel@users.noreply.github.com>
2023-07-08 23:40:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 var img = document.querySelector("#main_img");
 
+// Function to set the main image and trigger auto-change after 3 seconds
 var mainImg = function () {
   img.src = "assets/succulents-2.jpg";
   setTimeout(mainImgAuto, 3000);
 };
 
+// Function to display an alert indicating an empty cart
 var emptyCart = function () {
   alert("Your Cart Is Empty!");
 };
 
+// Function to set the main image to the default image
 var mainImgAuto = function () {
   return (img.src = "assets/succulents-1.jpg");
 };
 
+// Function to remove the popup element when the user accepts
 var iAccept = function () {
   document.querySelector("#popup").remove();
 };
